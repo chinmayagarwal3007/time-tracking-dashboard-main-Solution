2556cab2aae450bf4b256f64bec5b2b7bc333955
Chinmay Agarwal <76059604+chinmayagarwal3007@users.noreply.github.com>
2021-12-06 19:39:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+function weakFunction() {
+    document.getElementById("work_time").innerHTML = "32hrs";
+    document.getElementById("play_time").innerHTML = "10hrs";
+    document.getElementById("study_time").innerHTML = "4hrs";
+    document.getElementById("social_time").innerHTML = "5hrs";
+    document.getElementById("exercise_time").innerHTML = "4hrs";
+    document.getElementById("self_care_time").innerHTML = "2hrs";
+
+    document.getElementById("work_previous_time").innerHTML = "Last week - 36hrs";
+    document.getElementById("play_previous_time").innerHTML = "Last week - 8hrs";
+    document.getElementById("study_previous_time").innerHTML = "Last week - 7hrs";
+    document.getElementById("social_previous_time").innerHTML = "Last week - 10hrs";
+    document.getElementById("exercise_previous_time").innerHTML = "Last week - 5hrs";
+    document.getElementById("self_care_previous_time").innerHTML = "Last week - 2hrs";
+
+    document.getElementById("text_week").style.color = "white"
+    document.getElementById("text_daily").style.color = "hsl(235, 45%, 61%)"
+    document.getElementById("text_month").style.color = "hsl(235, 45%, 61%)"
+}
+
+function monthFunction() {
+    document.getElementById("work_time").innerHTML = "103hrs";
+    document.getElementById("play_time").innerHTML = "23hrs";
+    document.getElementById("study_time").innerHTML = "13hrs";
+    document.getElementById("social_time").innerHTML = "21hrs";
+    document.getElementById("exercise_time").innerHTML = "11hrs";
+    document.getElementById("self_care_time").innerHTML = "7hrs";
+
+    document.getElementById("work_previous_time").innerHTML = "Last month - 128hrs";
+    document.getElementById("play_previous_time").innerHTML = "Last month - 29hrs";
+    document.getElementById("study_previous_time").innerHTML = "Last month - 19hrs";
+    document.getElementById("social_previous_time").innerHTML = "Last month - 23hrs";
+    document.getElementById("exercise_previous_time").innerHTML = "Last month - 18hrs";
+    document.getElementById("self_care_previous_time").innerHTML = "Last month - 11hrs";
+
+
+    document.getElementById("text_month").style.color = "white"
+    document.getElementById("text_daily").style.color = "hsl(235, 45%, 61%)"
+    document.getElementById("text_week").style.color = "hsl(235, 45%, 61%)"
+}
+
+function dayFunction() {
+    document.getElementById("work_time").innerHTML = "5hrs";
+    document.getElementById("play_time").innerHTML = "1hr";
+    document.getElementById("study_time").innerHTML = "4hrs";
+    document.getElementById("social_time").innerHTML = "2hrs";
+    document.getElementById("exercise_time").innerHTML = "3hrs";
+    document.getElementById("self_care_time").innerHTML = "6hrs";
+
+    document.getElementById("work_previous_time").innerHTML = "Yesterday - 7hrs";
+    document.getElementById("play_previous_time").innerHTML = "Yesterday - 2hrs";
+    document.getElementById("study_previous_time").innerHTML = "Yesterday - 1hr";
+    document.getElementById("social_previous_time").innerHTML = "Yesterday - 3hrs";
+    document.getElementById("exercise_previous_time").innerHTML = "Yesterday - 1hr";
+    document.getElementById("self_care_previous_time").innerHTML = "Yesterday - 1hr";
+
+
+    document.getElementById("text_daily").style.color = "white"
+    document.getElementById("text_week").style.color = "hsl(235, 45%, 61%)"
+    document.getElementById("text_month").style.color = "hsl(235, 45%, 61%)"
+}
